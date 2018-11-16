3571187197d2e930f82aa9cb12eed8e5338ecaef
Ryan <idokamaroq@gmail.com>
2018-11-15 19:03:31 -0600

    Started implementing Database

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,33 @@
 public class Database {
+    private Item[] inventoryList;
+    private Transaction[] transactionLogList;
+    private Account[] accountList;
+
+    public Database(){
+        
+    }
+
+    public Item[] getInventoryList(){
+        return this.inventoryList;
+    }
+
+    public void setInventoryList(Item[] items){
+        this.inventoryList = items;
+    }
+
+    public Transaction[] getTransactionLogList(){
+        return this.transactionLogList;
+    }
+
+    public void setTransactionLogList(Transaction[] transactions){
+        this.transactionLogList = transactions;
+    }
+
+    public Account[] getAccountList(){
+        return this.accountList;
+    }
+
+    public void setAccountList(Account[] accounts){
+        this.accountList = accounts;
+    }
 }
