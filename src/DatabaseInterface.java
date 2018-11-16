89d1c3dc016b6f71fd85ee6945d68bf2aec673b9
Ryan <thechanceofrain@gmail.com>
2018-11-16 09:47:14 -0600

    Implemented DatabaseInterface.updateInv()

--- src/DatabaseInterface.java
+++ src/DatabaseInterface.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseInterface {
+    public static void updateInv(Item newItem){
+        Database database = new Database();
+        List<Item> inventory = database.getInventoryList();
+        int i = 0;
+        boolean found = false;
+        while(!found && i < inventory.size()){
+            found = inventory.get(i).name.equals(newItem.name));
+        }
+        if(found) {
+            inventory.set(i, newItem);
+        }
+        else {
+            inventory.add(newItem);    
+        }
+        database.setInventoryList(inventory);
+    }
+
+    public static void printReport(){
+        //TODO
+    }
+
+    public static void printReceipt(Transaction transaction, Account account){
+        //TODO
+    }
 }
