f5c875c33dc413bababee3e9d01afa56b9e6d6d1
Ryan <thechanceofrain@gmail.com>
2018-11-17 13:44:02 -0600

    Get rid of DatabaseInterface (move to EmployeeInterface

--- src/EmployeeInterface.java
+++ src/EmployeeInterface.java
@@ -1,4 +1,30 @@
-public class EmployeeInterface
+public static class EmployeeInterface
 {
-//TODO
+    public static void updateItem(){
+        //TODO
+        // updateInv(item);
+    }
+
+    public static void restockItem(){
+        //TODO
+        // updateInv(item);
+    }
+
+    private static void updateInv(Item newItem){
+        Database database = new Database();
+        List<Item> inventory = database.getInventoryList();
+        int i = 0;
+        boolean found = false;
+        while(!found && i < inventory.size()){
+            found = inventory.get(i).name.toUpperCase().equals(newItem.name.toUpperCase());
+            i++;
+        }
+        if(found) {
+            inventory.set(i, newItem);
+        }
+        else {
+            inventory.add(newItem);    
+        }
+        database.setInventoryList(inventory);
+    }
 }
