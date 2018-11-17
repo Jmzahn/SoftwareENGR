1a05c181dd7ce62985bd53dc3d9e965a07c84366
mainpc <jmzahn@satx.rr.com>
2018-11-17 17:06:51 -0600

    made DatabaseInterface hold a static Database, added getDatabase to it

--- src/DatabaseInterface.java
+++ src/DatabaseInterface.java
@@ -1,21 +1,26 @@
 
 
 public class DatabaseInterface {
-    public static Database database = new Database();
+    private static Database database = new Database();
 
-    public static Report prepareDailyReport(){//called by BusinessLogic
+    public static Database getDatabase(){
+        return database;
+    }
+
+
+    static Report prepareDailyReport(){//called by BusinessLogic
         Report d = new Report(ReportType.DAILY);
         d.setLines();
         return d;
     }
 
-    public static Report prepareInventoryReport(){//called by BusinessLogic
+    static Report prepareInventoryReport(){//called by BusinessLogic
         Report i = new Report(ReportType.INVENTORY);
         i.setLines();
         return i;
     }
 
-    public static Report prepareReceipt(Transaction transaction, Account account){//called by payCash and payCard
+    static Report prepareReceipt(Transaction transaction, Account account){//called by payCash and payCard
         Report r = new Report(ReportType.RECEIPT);
         r.setLines();
         return r;
