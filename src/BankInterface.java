df39cbeba5710cb915401c6b072c65faf8836078
TheChrisGee <36210022+TheChrisGee@users.noreply.github.com>
2018-11-25 15:10:28 -0600

    Added outline

--- src/BankInterface.java
+++ src/BankInterface.java
@@ -1,2 +1,56 @@
-public class BankInterface {//TODO
+import java.util.Scanner;
+
+public class BankInterface {
+    public static void GetCardNo(){
+        String  DebitAnswer, cardNo, PIN;
+        int i = 0;
+        String[] StoredCardNo = {"12345678", "23456790", "345678901"};
+        String[] StoredPINNo = {"1234", "2345", "3456"};        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Card Number:");
+        cardNo = input.nextLine();
+        for(i=0;i<3;i++){
+            if( cardNo.equals(StoredCardNo[i]))
+            {
+                System.out.println("Is this a debit card? Y/N");
+                DebitAnswer = input.nextLine();
+                if(DebitAnswer.equals("Y") || (DebitAnswer.equals("y")))
+                {
+                    System.out.println("Enter PIN number:");
+                    PIN = input.nextLine();
+                    if(PIN.equals(StoredPINNo[i]))
+                    {
+                        System.out.println("Card Accepted");
+                        getReceipt(cardNo);
+                    }
+                }
+                else
+                {
+                    System.out.println("Card Accepted");
+                    getReceipt(cardNo);
+                }
+            }
+            else{
+                System.out.println("Card not recognized");
+
+            } 
+
+        }
+
+
+    }
+    public String getReceipt(string cardNo)
+    {
+        lastFour = cardNo.substring(cardNo.length() - 4);
+        return System.out.println("Your receipt:\nLast four digits: " + lastFour);
+
+    }
+
+
+public static void main(String[] args)
+{
+    GetCardNo();
 }
+    
+}
+
